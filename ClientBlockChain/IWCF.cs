e0a21c9d18383b977b77a585a653a62cf91c519c
Kojee <marco.filippozzi98@gmail.com>
2017-03-14 16:18:33 +0100

    Implementata comunicazione WCF con processo di backend per la creazione e loading dei keystore al click dei pulsanti. Aggiunta interfaccia IWCF per i servizi da richiamare

---
+++ ClientBlockChain/IWCF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientBlockChain
+{
+    [ServiceContract]
+    interface IWCF
+    {
+        [OperationContract]
+        void LoadKeyStore(string name, string password);
+        [OperationContract]
+        void GenerateKeyStore(string name, string password);
+        
+    }
+}
--- ClientBlockChain/MainWindow.xaml.cs
+++ ClientBlockChain/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,40 @@ namespace ClientBlockChain
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+
+        [DllImport("Kernel32")]
+        public static extern void FreeConsole();
+        private NetNamedPipeBinding mBinding;
+        private EndpointAddress mEp;
+        private IWCF mChannel;
+        private string mAddress = "net.pipe://localhost/gorillacoding/IPCTest";
+
         public MainWindow()
         {
             InitializeComponent();
+            mBinding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            mEp = new EndpointAddress(mAddress);
+            mChannel = ChannelFactory<IWCF>.CreateChannel(mBinding, mEp);
+            AllocConsole();
+            Console.WriteLine("Client Connected");
+        }
+
+        private void Button_Load_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Insert [name] [password]");
+            string command = Console.ReadLine();
+            string[] exCommand = command.Split(' ');
+            mChannel.LoadKeyStore(exCommand[0], exCommand[1]);
+        }
+
+        private void Button_Create_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Insert [name] [password]");
+            string command = Console.ReadLine();
+            string[] exCommand = command.Split(' ');
+            mChannel.GenerateKeyStore(exCommand[0], exCommand[1]);
         }
     }
 }
